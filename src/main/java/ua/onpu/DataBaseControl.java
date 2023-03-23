00c97bc9d31de6c65be8567f1e92c1085902e268
Vladislav <stanl2062@gmail.com>
2023-03-23 13:44:33 +0200

    statements processing

--- src/main/java/ua/onpu/DataBaseControl.java
+++ src/main/java/ua/onpu/DataBaseControl.java
@@ -1,6 +1,7 @@
 package ua.onpu;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import ua.onpu.model.*;
@@ -19,13 +20,31 @@ public class DataBaseControl {
     @Autowired
     private AssigmentRepository assigmentRepository;
 
-    public void makeRemind(Message message) {
+    public void makeRemind(Message message) throws DataAccessException {
         Task task = new Task();
         task.setTaskText(message.getText());
         taskRepository.save(task);
 
         Assigment assigment = new Assigment();
 
+        User user = userRepository.findById(message.getChatId()).get();
+
+        assigment.setUser(user);
+        assigment.setTask(task);
+        assigmentRepository.save(assigment);
+    }
+
+    public List<Task> showList(Message message) {
+        List<Assigment> assigments = assigmentRepository.findAllByUserChatId(message.getChatId());
+        List<Task> taskList = new ArrayList<>();
+        for (Assigment a : assigments) {
+            taskList.add(a.getTask());
+        }
+
+        return taskList;
+    }
+
+    public void registerUser(Message message) {
         if (userRepository.findById(message.getChatId()).isEmpty()) {
             User user = new User();
             user.setChatId(message.getChatId());
@@ -33,27 +52,6 @@ public class DataBaseControl {
             user.setRegisteredAt(LocalDateTime.now());
 
             userRepository.save(user);
-
-            assigment.setUser(user);
-            assigment.setTask(task);
-            assigmentRepository.save(assigment);
-        } else {
-            User user = userRepository.findById(message.getChatId()).get();
-
-            assigment.setUser(user);
-            assigment.setTask(task);
-            assigmentRepository.save(assigment);
         }
     }
-
-    public void showList(Message message){
-        List<Assigment> assigments = assigmentRepository.findAllByUserChatId(message.getChatId());
-        List<Task> taskList = new ArrayList<>();
-        for(Assigment a : assigments){
-            taskList.add(a.getTask());
-        }
-
-        System.out.println(taskList);
-    }
-
 }
--- src/main/java/ua/onpu/TelegramBot.java
+++ src/main/java/ua/onpu/TelegramBot.java
@@ -4,6 +4,8 @@ package ua.onpu;
 import com.vdurmont.emoji.EmojiParser;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
@@ -12,8 +14,11 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ua.onpu.configuration.BotConfiguration;
@@ -58,31 +63,41 @@ public class TelegramBot extends TelegramLongPollingBot {
             String command = update.getMessage().getText();
             switch (state) {
                 case START:
-                    if(command.equals("/start")){
-                        startCommand(chatId, EmojiParser.parseToUnicode("Hello, i'm ReminderBOT. Please choose the option" + ":blush:"));
-                    }
-                    else if (command.equals("Reminder list")){
-                        state = Statements.VIEW;
-                        dataBaseControl.showList(update.getMessage());
-                    }
-                    else if(command.equals("Make a new reminder")){
-                        state = Statements.CREATE;
-                        sendMessage(chatId, "Write your reminder");
-                    }
-                    else if(command.equals("Delete reminder")) {
-                        state = Statements.DELETE_CONFIRMATION;
-                        sendMessage(chatId, "Select the reminder you want to delete");
-                        dataBaseControl.showList(update.getMessage());
-                    } else {
-                        startCommand(chatId, EmojiParser.parseToUnicode("Please choose the option" + ":blush:"));
+                    switch (command) {
+                        case "/start":
+                            dataBaseControl.registerUser(update.getMessage());
+                            sendMessage(chatId, EmojiParser.parseToUnicode("Hello, i'm ReminderBOT. Please choose the option" + ":blush:"), startStateKeyboard());
+                            break;
+                        case "Reminder list":
+                            state = Statements.VIEW;
+                            sendMessage(chatId, "Your reminder list", viewStateKeyboard(dataBaseControl.showList(update.getMessage())));
+                            break;
+                        case "Make a new reminder":
+                            state = Statements.CREATE;
+                            sendMessage(chatId, "Write your reminder", new ReplyKeyboardRemove(true));
+                            break;
+                        case "Delete reminder":
+                            state = Statements.DELETE_CONFIRMATION;
+                            sendMessage(chatId, "Select the reminder you want to delete");
+                            dataBaseControl.showList(update.getMessage());
+                            break;
+                        default:
+                            sendMessage(chatId, EmojiParser.parseToUnicode("Please choose the option" + ":blush:"), startStateKeyboard());
+                            break;
                     }
                     break;
                 case CREATE:
-                    dataBaseControl.makeRemind(update.getMessage());
+                    try {
+                        dataBaseControl.makeRemind(update.getMessage());
+                    } catch (DataAccessException e) {
+                        log.error(e.getMessage());
+                    }
+                    sendMessage(chatId, "Done!", startStateKeyboard());
                     state = Statements.START;
                     break;
                 case VIEW:
                     sendMessage(chatId, "VIEW");
+                    state = Statements.START;
                     break;
                 case EDIT:
                     sendMessage(chatId, "EDIT");
@@ -97,9 +112,38 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
+    private ReplyKeyboard viewStateKeyboard(List<Task> list) {
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
 
-    // Стартовые кнопки
-    private void startCommand(String chatId, String text) {
+        List<List<InlineKeyboardButton>> rowButton = new ArrayList<>();
+
+        if(list.isEmpty()){
+            InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
+            List<InlineKeyboardButton> buttonList = new ArrayList<>();
+
+            inlineKeyboardButton.setText("EMPTY");
+            buttonList.add(inlineKeyboardButton);
+            rowButton.add(buttonList);
+        }
+
+        int i = 1;
+        for (Task t : list) {
+            InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
+            List<InlineKeyboardButton> buttonList = new ArrayList<>();
+
+            inlineKeyboardButton.setText(i++ + ". " + t.getTaskText());
+            inlineKeyboardButton.setCallbackData(Long.toString(t.getTaskId()));
+
+            buttonList.add(inlineKeyboardButton);
+            rowButton.add(buttonList);
+
+        }
+
+        inlineKeyboardMarkup.setKeyboard(rowButton);
+        return inlineKeyboardMarkup;
+    }
+
+    private ReplyKeyboard startStateKeyboard() {
         ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
 
         List<KeyboardRow> keyboardRows = new ArrayList<>();
@@ -121,7 +165,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         replyKeyboardMarkup.setKeyboard(keyboardRows);
         replyKeyboardMarkup.setResizeKeyboard(true);
 
-        sendMessage(chatId, text, replyKeyboardMarkup);
+        return replyKeyboardMarkup;
     }
 
     private void sendMessage(String chatId, String text, ReplyKeyboard keyboardButtons) {
